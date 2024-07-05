f256f528ee4ca9b17b38b74b782d6f3ba205dd81
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-07-05 16:26:59 +0200

    fix output

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -35,7 +35,6 @@ fi
 
 if [[ -n ${FIX_VERSION} ]]; then
   echo "fixing versions...."
-  IFS=', ' read -r -a VERSIONS <<< $(${GP}sed -n 's/version_list: //p' ../../pyqgis_conf.yml)
   HTML=""
   for v in "${VERSIONS[@]}"; do
     HTML="${HTML}\n      \n        <dd><a href=\"https://qgis.org/pyqgis/${v}\">${v}</a></dd>"
