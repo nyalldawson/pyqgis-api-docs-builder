bfdff8e81ce29abd5371deb14caba0c643c24845
Denis Rouzaud <denis.rouzaud@gmail.com>
2024-07-12 10:59:26 +0200

    fix links
    fixes #143

--- scripts/make_api_rst.py
+++ scripts/make_api_rst.py
@@ -62,12 +62,12 @@ def ltr_tag(v):
 current_stable = cfg["current_stable"]
 current_ltr = cfg["current_ltr"]
 current_stable_minor = int(current_stable.split(".")[1]) + 2  # '3.38' => 40
-old_versions_links = ", ".join(
+old_versions_links = ", ".join(reversed(
     [
-        f"`3.{v} <https://github.com/qgis/pyqgis/releases/download/3.{v}/3.{v}.zip>`_"
+        f"`3.{v} <https://github.com/qgis/pyqgis-api-docs-builder/releases/download/3.{v}/pyqgis-docs-3.{v}.zip>`_"
         for v in range(0, current_stable_minor, 2)
     ]
-)
+))
 
 
 # Make sure :numbered: is only specified in the top level index - see
