d28d6fcade01176391ac9ac0ee6517b7f0308f25
Nyall Dawson <nyall.dawson@gmail.com>
2024-09-11 11:00:13 +1000

    Link Qt base class objects to Qt c++ doc pages
    We use the c++ qt docs, because the PyQt docs don't have any real
    useful detail.
    
    Fixes #175

--- conf.in.py
+++ conf.in.py
@@ -132,6 +132,7 @@ current_ltr = str(cfg["current_ltr"])
 version_list = ("master", current_stable, current_ltr)
 
 graphviz_output_format = "svg"
+qt_docs_url_base = cfg["qt_docs_url_base"]
 
 url = cfg["pyqgis_url"]
 if not url.endswith("/"):
